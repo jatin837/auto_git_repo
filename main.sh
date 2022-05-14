19e5faccf75ada380280acf97343d55e04431d0d
Jatin Pandey <pandeyjatin47@gmail.com>
2022-05-14 12:37:17 +0530

    automation script works

--- main.sh
+++ main.sh
@@ -2,7 +2,6 @@
 
 REMOTE_REPOS=$(git remote)
 
-# arrIN=(${remote_repo// / })
 
 THIS_PROJ_DIR=$(pwd)
 cd ../
@@ -10,13 +9,17 @@ DIR_LOC=$(pwd)
 cd $THIS_PROJ_DIR
 
 PROJ_NAME=${THIS_PROJ_DIR:${#DIR_LOC}+1}
-echo $PROJ_NAME
+GH_USER_NAME=jatin837
+
 
 curl -i -H "Authorization: token $GH_KEY" \
     -d "{
 					\"name\": \"${PROJ_NAME}\",
-					\"auto_init\": true,
-					\"private\": true,
-					\"gitignore_template\": \"nanoc\"
+					\"auto_init\": false,
+					\"private\": true
       }" \
     https://api.github.com/user/repos
+
+git remote add origin git@github.com:$GH_USER_NAME/$PROJ_NAME.git
+git branch -M main
+git push -u origin main
