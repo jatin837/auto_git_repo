8e491e77de8155479cc465b28aff498c622c585d
Jatin Pandey <pandeyjatin47@gmail.com>
2022-05-14 12:21:29 +0530

    resolved
    use ""

--- main.sh
+++ main.sh
@@ -13,10 +13,10 @@ PROJ_NAME=${THIS_PROJ_DIR:${#DIR_LOC}+1}
 echo $PROJ_NAME
 
 curl -i -H "Authorization: token $GH_KEY" \
-    -d '{
-					"name": "${PROJ_NAME}",
-					"auto_init": true,
-					"private": true,
-					"gitignore_template": "nanoc"
-      }' \
+    -d "{
+					\"name\": \"${PROJ_NAME}\",
+					\"auto_init\": true,
+					\"private\": true,
+					\"gitignore_template\": \"nanoc\"
+      }" \
     https://api.github.com/user/repos
